e8544e0cc562f660bf3aa842ad537229f7b8ea79
Samantha <samanthabarrueta@gmail.com>
2020-07-05 13:24:24 -0500

    api routes get / post

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,3 +1,4 @@
+const data = require('../db/db.json');
 const path = require('path');
 const fs = require('fs');
 
@@ -10,4 +11,19 @@ module.exports = (app) => {
         res.json(status);
     });
 
+    app.get('/api/notes', (req, res) => {
+        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) => {
+            if (err) throw err;      
+        });
+        res.json(data);
+    });
+
+    app.post('/api/notes', (req, res) => {
+        const note = req.body;
+        data.push(note);
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), (err, data) => {
+            if (err) throw err;      
+        });
+        res.send(data);
+    });
 }
