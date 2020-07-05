895a01f6afdd70fd7d66f7acbe908c0840d629bb
Samantha <samanthabarrueta@gmail.com>
2020-07-05 11:55:36 -0500

    update server.js
    hit endpoints

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,20 @@
 const express= require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const routes = require('./routes/apiroutes')
+const routes = require('./routes/apiroutes');
+const htmlRoutes = require('./routes/htmlroutes');
 
 const app = express();
 const PORT = 9000;
 
+const run = (app) => {
+    htmlRoutes(app);
+    routes(app);
+}
+
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-routes.route(app);
+run(app);
 
 app.listen(PORT, () => console.log(`API is listening on port ${PORT}`));
