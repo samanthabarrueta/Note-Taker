c0ecff137763b91c40903b93c4af5b277bc004b9
Samantha <samanthabarrueta@gmail.com>
2020-07-06 19:47:02 -0500

    delete by id

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,6 +1,7 @@
 const data = require('../db/db.json');
 const path = require('path');
 const fs = require('fs');
+const { request } = require('http');
 
 module.exports = (app) => {
 
@@ -27,4 +28,20 @@ module.exports = (app) => {
         });
         res.send(data);
     }); 
+
+    app.delete('/api/notes/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) => {
+            if (err) throw err;  
+        }); 
+            if (id === data[id].id) {
+                const newData = data.splice(id, 1);
+                res.send(newData);
+            }
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), (err, data) => {
+            if (err) throw err;      
+        });
+        
+    });
+    
 }
